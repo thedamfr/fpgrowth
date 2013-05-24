94cec578d6b371ee9049399539159c1eb40594c8
Lucie Castier <lucie.castier@gmail.com>
2013-05-24 23:19:40 +0200

    add Test for FP Tree

--- test/tc_fp_tree.rb
+++ test/tc_fp_tree.rb
@@ -42,15 +42,31 @@ class TestFpTree < Test::Unit::TestCase
     assert_not_nil( fp_tree.root )
     assert_instance_of( FpGrowth::FpTree::Node , fp_tree.root )
     assert_instance_of( Hash , fp_tree.heads )
-    #assert_equal( {1,2} , fp_tree.supports )
     assert( fp_tree.heads.has_key?('a') , "a n'existe pas")
     assert( fp_tree.heads.has_key?('b') ,"b n'existe pas !" )
-
+    assert_equal( 2 , fp_tree.heads.length )
+    assert( fp_tree.supports.has_key?('a') , "a n'existe pas")
+    assert( fp_tree.supports.has_key?('b') ,"b n'existe pas !" )
+    assert_equal( 2 , fp_tree.supports.length )
 
   end
 
   # test item_oder_lookup
   def test_item_oder_lookup
+
+    # look up with fp_tree nul
+    fp_tree = nil
+    # no arguments
+    assert_nothing_raised {fp_tree = FpGrowth::FpTree::FpTree.new()}
+    lookup = fp_tree.item_order_lookup
+    assert_equal( {} , lookup )
+
+    # look up with fp_tree non null
+    support =  { 'a' => 1, 'b' => 2}
+    assert_nothing_raised {fp_tree = FpGrowth::FpTree::FpTree.new(support)}
+    lookup = fp_tree.item_order_lookup
+    assert_equal( 0  , lookup['a'] )
+    assert_equal( 1  , lookup['b'] )
 
   end
 
