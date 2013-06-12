853027f824aeb1727343249ac8342b8f4c4dfb11
thedamfr <dam.cavailles@laposte.net>
2013-06-12 00:05:49 -0400

    added easy interface

--- lib/fpgrowth.rb
+++ lib/fpgrowth.rb
@@ -1,8 +1,20 @@
 require "fpgrowth/version.rb"
-
-module FpGrowth
-  # Your code goes here...
-end
-
 require 'fpgrowth/fp_tree'
 require 'fpgrowth/miner'
+
+module FpGrowth
+  def self.mine(transactions, threshold=1)
+    td_fp_growth(transactions, threshold)
+  end
+
+  def self.fp_growth(transactions, threshold=1)
+    fp_tree = FpTree.build(transactions, threshold)
+    Miner.fp_growth(fp_tree)
+  end
+
+  def self.td_fp_growth(transactions, threshold=1)
+    fp_tree = FpTree.build(transactions, threshold)
+    Miner.td_fp_growth(fp_tree)
+  end
+
+end
